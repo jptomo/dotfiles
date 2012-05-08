91ae419d76adb74742f47bd1c54ae471e1c1687f
Tomohiro Nakamura <quickness.net@gmail.com>
2012-05-08 22:02:07 +0900

    gem用設定追加

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DOT_FILES=( .gitconfig .gitignore .hgrc .hgrc .hgeol .hgignore .zshrc .zsh.alias \
-.zsh_profile .zsh.mode .tmux.conf .inputrc .vimrc)
+.zsh_profile .zsh.mode .tmux.conf .inputrc .vimrc .gemrc)
 BAKDIR=backup/`date +%Y%m%d%H%M%S`
 
 CURDIR=$(cd $(dirname $0); pwd)

ea41b947573d0f891e0b1eb79dda601855ec2edb
Tomohiro Nakamura <quickness.net@gmail.com>
2015-07-26 12:42:00 +0900

    update misc/Microsoft.PowerShell_profile.ps1

--- misc/Microsoft.PowerShell_profile.ps1
+++ misc/Microsoft.PowerShell_profile.ps1
@@ -2,106 +2,105 @@
 $OutputEncoding = [Text.Encoding]::Default
 $ENV:LANG = "ja_JP.CP932"
 
-# create function touch
 Function touch()
 {
-  Param([string]$filename)
+    Param([string]$filename)
 
-  Process {New-Item $filename -itemType File}
+    Process {New-Item $filename -itemType File}
 }
 
 Function which()
 {
-  Param([string]$name)
+    Param([string]$name)
 
-  Process
-  {
-    $cmd = Get-Command $name
-    $def = $cmd.Definition
-    IF ($cmd.CommandType -ne 'Alias')
+    Process
     {
-      Write-Host $def
+        $cmd = Get-Command $name
+        $def = $cmd.Definition
+        IF ($cmd.CommandType -ne 'Alias')
+        {
+            Write-Host $def
+        }
+        Else
+        {
+            Write-Host "(Alias) $def"
+        }
     }
-    Else
-    {
-      Write-Host "(Alias) $def"
-    }
-  }
 }
 
 Function env()
 {
-  Process {Get-ChildItem ENV:}
+    Process {Get-ChildItem ENV:}
 }
 
 function _reverse_check() {
-  Param(
-    $path,
-    $dir_name
-  )
+    Param(
+        $path,
+        $dir_name
+    )
 
-  Process
-  {
-    $_path = $(Convert-Path -Path $path)
-    $_root = $(Join-Path (Split-Path -Path $_path -Qualifier) '\')
-    if (Test-Path (Join-Path $_path $dir_name)) {
-      return $TRUE;
-    } elseif ($_root -eq $_path) {
-      return $FALSE;
-    } else {
-      $_parent = Split-Path -Path $_path -Parent
-      _reverse_check $_parent $dir_name
+    Process
+    {
+        $_path = $(Convert-Path -Path $path)
+        $_root = $(Join-Path (Split-Path -Path $_path -Qualifier) '\')
+        if (Test-Path (Join-Path $_path $dir_name)) {
+            return $TRUE;
+        } elseif ($_root -eq $_path) {
+            return $FALSE;
+        } else {
+            $_parent = Split-Path -Path $_path -Parent
+            _reverse_check $_parent $dir_name
+        }
     }
-  }
 }
 
 function git_branch() {
-  Process
-  {
-    if (-Not (_reverse_check $PWD.Path '.git'))
+    Process
     {
-      return
-    }
+        if (-Not (_reverse_check $PWD.Path '.git'))
+        {
+            return
+        }
 
-    git branch 2>$null |
-    ? { -not [System.String]::IsNullOrEmpty($_.Split()[0]) } |
-    % { $bn = $_.Split()[1]
-        Write-Output "(git:$bn) " }
-  }
+        git branch 2>$null |
+        ? { -not [System.String]::IsNullOrEmpty($_.Split()[0]) } |
+        % { $bn = $_.Split()[1]
+                Write-Output "(git:$bn) " }
+    }
 }
 
 function hg_branch() {
-  Process
-  {
-    if (-Not (_reverse_check $PWD.Path '.hg'))
+    Process
     {
-      return
-    }
+        if (-Not (_reverse_check $PWD.Path '.hg'))
+        {
+            return
+        }
 
-    hg branch 2>$null | % { Write-Output "(hg:$_) " }
-  }
+        hg branch 2>$null | % { Write-Output "(hg:$_) " }
+    }
 }
 
 # return current directory
 # with $ENV:HOME to ~
 function _get_pwd() {
-  $path = $(Get-Location).Path
-  if ($path.IndexOf($ENV:HOME) -eq 0) {
-    $path = Join-Path '~' ($PWD.ProviderPath.Remove(0, ($ENV:HOME).Length))
-  }
-  return $path
+    $path = $(Get-Location).Path
+    if ($path.IndexOf($ENV:HOME) -eq 0) {
+        $path = Join-Path '~' ($PWD.ProviderPath.Remove(0, ($ENV:HOME).Length))
+    }
+    return $path
 }
 
-<#
-function prompt {
-  Process {
-    $git_b = git_branch
-    $hg_b = hg_branch
-    # short home path
-    return "$($git_b)$($hg_b)[$(Get-Date -Format 'yyyy/mm/dd hh:mm:ss')] PS $(_get_pwd)`n> "
-  }
+Function Global:Prompt {
+    $realLASTEXITCODE = $LASTEXITCODE
+    $Host.UI.RawUI.ForegroundColor = "White"
+    Write-Host $pwd.ProviderPath -NoNewLine -ForegroundColor Green
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    Write-Host "`n" -NoNewLine -ForegroundColor "DarkGray"
+    return "[$(Get-Date -Format 'yyyy/mm/dd hh:mm:ss')] `nPS> "
+    # $git_b, $hg_b = git_branch, hg_branch
+    # return "$($git_b)$($hg_b)[$(Get-Date -Format 'yyyy/mm/dd hh:mm:ss')] PS $(_get_pwd)`n> "
 }
-#>
 
 # vim
 [Environment]::SetEnvironmentVariable("VIM", $null, "User")
@@ -112,78 +111,41 @@ Set-Alias -name vi -value vim.exe
 # plink
 #Set-Alias -name plink -value "<plink path>"
 
-# make cert 
-#Set-Alias -name makecert -value "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\makecert.exe"
-# If you modify this file. Run below command:
-# PS > $cert = Get-ChildItem cert:\CurrentUser\My -codesigning
-# PS > Set-AuthenticodeSignature Profile.ps1 $cert
-
 # Python
 #$ENV:DISTUTILS_USE_SDK = 1
 $ENV:PATHEXT += ";.PY"
-
-# SDKs
-<# & {
-  $script = "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
-  $parameters = "amd64"
-  $tempFile = [IO.Path]::GetTempFileName()
-  cmd.exe /C " `"$script`" $parameters && SET > `"$tempFile`" "
-  Get-Content $tempFile | % {
-    if($_ -match "^(.*?)=(.*)$") {
-      Set-Item -Path "ENV:\$($matches[1])" -Value $matches[2]
-    }
-  }
-  Remove-Item $tempFile
-} #>
-<# & {
-  $script = "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd"
-  $parameters = "/Release /x64 /win7"
-  $tempFile = [IO.Path]::GetTempFileName()
-  cmd.exe /E:ON /V:ON /T:0E /C " `"$script`" $parameters && SET > `"$tempFile`" "
-  Get-Content $tempFile | % {
-    if($_ -match "^(.*?)=(.*)$") {
-      Set-Item -Path "ENV:\$($matches[1])" -Value $matches[2]
-    }
-  }
-  Remove-Item $tempFile
-} #>
-<# & {
-  $script = "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
-  $parameters = "x86_amd64"
-  $tempFile = [IO.Path]::GetTempFileName()
-  cmd.exe /C " `"$script`" $parameters && SET > `"$tempFile`" "
-  Get-Content $tempFile | % {
-    if($_ -match "^(.*?)=(.*)$") {
-      Set-Item -Path "ENV:\$($matches[1])" -Value $matches[2]
-    }
-  }
-  Remove-Item $tempFile
-} #>
-# & cmd /c "COLOR 07"   # backup color
-
-# ### SetUp PATH ###
-#
-# Python
-# $ENV:MSSDK = "C:\Program Files\Microsoft SDKs\Windows\v7.1"
 # & "~\.local\venv\Scripts\activate.ps1"
+#
+# ### SetUp PATH ###
+
+# Mingw64
+$ENV:MSYSTEM = "MINGW64"
+$ENV:MSYSCON = "path\to\Cmder.exe"
+$ENV:PATH += ";C:\msys64\mingw64\lib;C:\msys64\mingw64\bin"
+$ENV:MINGW_MOUNT_POINT = "C:\msys64\mingw64"
+$ENV:PKG_CONFIG_PATH = "${MINGW_MOUNT_POINT}\lib\pkgconfig;${MINGW_MOUNT_POINT}\share\pkgconfig"
+$ENV:ACLOCAL_PATH = "${MINGW_MOUNT_POINT}\share\aclocal;C:\msys64\usr\share\aclocal"
+
+# VS2015
+#Import-Module WintellectPowerShell
+#Invoke-CmdScript -script "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
 
 # something else
 $cnv = {
-  param($path)
+    param($path)
 
-  return $path
+    return $path
 }
 $ENV:PATH = ($ENV:PATH -split ";" | ? {$_ -ne ''} | % {$_.Trim() -replace "/", "\"}) -join ";"
 $ENV:PATHEXT = ($ENV:PATHEXT -split ";" | ? {$_ -ne ''} | % {&$cnv $_.Trim().ToUpper()}) -join ";"
+Remove-Variable cnv
 
-#Set-Location "~\Documents"
+#Import-Module Pscx
 
-Remove-Variable path, cnv
+#Import-Module PoshUtil
+#Import-Module PoshGit2
 
 # PowerShell ReadLine
 # see https://github.com/lzybkr/PSReadLine#installation
 #Import-Module PSReadLine
 #Set-PSReadlineOption -EditMode Emacs
-
-# :fenc=cp932 ff=dos:
-# vim:ft=ps1 ts=2 sw=2 sts=2 fdl=0:

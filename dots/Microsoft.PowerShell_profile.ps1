220177624b4bae857499e38163cc4a6bcd60d5d3
Tomohiro Nakamura <quickness.net@gmail.com>
2016-11-17 01:41:08 +0900

    some

--- dots/Microsoft.PowerShell_profile.ps1
+++ dots/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,11 @@
 # cp932
 #
 # Init(Admin):
-#   > Install-Module -Scope CurrentUser -Name Pscx -RequiredVersion 3.2.1.0 -AllowClobber
+#   > Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 # Init:
-#   > Install-Module -Scope CurrentUser -Name posh-git -Force -AllowClobber
-#   > Install-Module -Scope CurrentUser -Name Pscx -Force -AllowClobber
+#   > Install-Module -Name posh-git -Scope CurrentUser -Force -AllowClobber
+#   > Install-Module -Name Pscx -Scope CurrentUser -Force -AllowClobber
+#   > Install-Module -Name WintellectPowerShell -Scope CurrentUser -Force -AllowClobber
 #
 
 # Load posh-git example profile
@@ -15,4 +16,16 @@
 #$ENV:Path = "$_vimPath;" + $Env:Path
 #Set-Alias -name vim -value "$_vimPath\vim.exe"
 
+#Import-Module WintellectPowerShell
+#Invoke-CmdScript -script "C:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbuildtools.bat" amd64
+
 . "$HOME\misc\dotfiles\misc\Microsoft.PowerShell_profile.ps1"
+
+Function Global:Prompt {
+    $realLASTEXITCODE = $LASTEXITCODE
+    $Host.UI.RawUI.ForegroundColor = "White"
+    Write-Host $pwd.ProviderPath -NoNewLine -ForegroundColor Green
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    Write-Host "`n" -NoNewLine -ForegroundColor "DarkGray"
+    return "$(git_branch)$(hg_branch)[$(Get-Date -Format 'yyyy/mm/dd hh:mm:ss')] PS $(_get_pwd)`n> "
+}
